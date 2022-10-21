526f17052b66061111121f529e1dc81802d073bb
Alex Anderson <chaoticgamer.exe@gmail.com>
2022-10-21 13:57:17 -0400

    Comments added for clarity/help

--- RobotFunctions.java
+++ RobotFunctions.java
@@ -1,8 +1,12 @@
 
 public class RobotFunctions {
-
+    //no main method, as we want RobotMain to be our program
+    //public and static, as we want other classes to access it, and the static keyword
+    //is to indicate that it is a method that should be accessed through the class, not as an object
+    //This way, instead of creating a RobotFunctions object, we only need a RobotFunctions class
     public static int CheckMotorRotation(Motor Motor) {
         return Motor.Encoder;
     }
     
+
 }
--- RobotMain.java
+++ RobotMain.java
@@ -1,7 +1,13 @@
 import java.lang.String;
-import java.io.*;
+//import the String object; data types in Java can be objects! 
+//For example, integers are their own objects with their own methods.  Integer objects are pointed to by
+//java variables. The variable 'int Two = 2' is pointing to an integer object that stores the value of 2.
+//This principle of OOP is not so important for the robot program but is important to understand the syntax.
 
+import java.io.*;
+//declare the class RobotMain
 public class RobotMain {
+    //this is the main method; the 'main' method is where the program runs
     public static void main(String[] args){
             Motor TestMotor = new Motor();
             System.out.println(String.valueOf(RobotFunctions.CheckMotorRotation(TestMotor)));
